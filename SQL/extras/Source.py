b1c7464b4f4184b1f8074d9fe400071f2dffc72f
qithoniq <119659334+qithoniq@users.noreply.github.com>
2023-02-19 20:02:46 +0300

    Add files via upload

---
+++ SQL/extras/Source.py
@@ -0,0 +1,11 @@
+import re
+import time
+from datetime import datetime
+from userbot import StartTime, iqthon
+from userbot.Config import Config
+from userbot.plugins import mention
+help1 = ("**♛ ⦙ كيفيه التنصيب :**")
+help2 = ("**♛ ⦙ قـائمـه الاوامـر :**\n**♛ ⦙ قنـاه السـورس :** @qithon\n**♛ ⦙ شـرح اوامـر السـورس : @uptidev**\n**♛ ⦙ شـرح فـارات السـورس : @uptidev** \n - اوامر الاونلاين تشتغل فقط في المجموعات ")
+TG_BOT = Config.TG_BOT_USERNAME
+TM = time.strftime("%I:%M")
+Sour = f"**♛ : version 7.6  𓇡.** \n♛ : me  {mention}  𓇡. \n**♛ : time  {TM}  𓇡.**\n**♛ : My Bot {TG_BOT} 𓇡.**\n**♛ : Source : @qithon  𓇡.**"
